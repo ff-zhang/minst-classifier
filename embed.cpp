845d45a8e9eecb8e78a8828adff67958e56eae05
Cole-Franklin1 <cole.franklin.personal@gmail.com>
2022-12-28 17:19:00 -0500

    Optimal 01-loss function 😎

--- embed.cpp
+++ embed.cpp
@@ -1,11 +1,12 @@
 //
 // Created by Felix Zhang on 2022-12-28.
-//
+
 
 #include <Eigen/Dense>
 
 #include "embed.h"
 
 VecLab embed(VecDom v, int y) {
-
+    VecLab psi = VecLab::Zero();
+    psi.segment<784>(784*y) = v;
 }
