893ff81bc22f3901ece6a8548b3b59a48e50104f
Cole-Franklin1 <cole.franklin.personal@gmail.com>
2022-12-28 18:17:40 -0500

    Fixed Typos

--- error.cpp
+++ error.cpp
@@ -11,13 +11,13 @@ double SGDLearner::TrainingError(Dataset &data) {
     for (auto iter = data.training.begin(); iter != data.training.end(); iter++) {
         accum += loss01(predict(std::get<0>(iter)), std::get<1>(iter));
     }
-    return accum / data.size;
+    return accum / data.trainingSize;
 }
 
 double SGDLearner::GeneralizationError(Dataset &data) {
     double accum = 0;
     for (auto iter = data.test.begin(); iter != data.test.end(); iter++) {
-        accum += loss01(predict(std::get<0>(iter), std::get<1>(iter));
+        accum += loss01(predict(std::get<0>(iter)), std::get<1>(iter));
     }
-    return accum / data.size;
+    return accum / data.testSize;
 }
