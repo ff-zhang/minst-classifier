ad0e8cc794a68764b10e7da52e04af0128f5fff9
Felix Zhang <felixfzhang@hotmail.com>
2023-01-02 11:11:43 -0800

    Fix error from merge

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     auto* mnist = new Dataset<NUM_TRAIN, NUM_TEST>(TRAIN_IMAGES, TRAIN_LABELS,TEST_IMAGES,
                                                    TEST_LABELS);
 
-    SGDLearner* model = new SGDLearner(0.1);
+    auto* model = new SGDLearner(0.1);
     model->train(*mnist, 20);
 
     std::cout << model->getWeights() << ",\n\n";
